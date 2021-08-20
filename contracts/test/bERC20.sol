0120d0cff8c4050017e6ccf282321992d8675d6b
Yuriy <yuriy77k@gmail.com>
2021-08-20 22:57:10 +0300

    rename files

--- contracts/test/bERC20.sol
+++ contracts/test/bERC20.sol
@@ -2,7 +2,7 @@ pragma solidity =0.5.16;
 
 import '../BSwapV2ERC20.sol';
 
-contract UniERC20 is BSwapV2ERC20 {
+contract bERC20 is BSwapV2ERC20 {
     constructor(uint _totalSupply) public {
         _mint(msg.sender, _totalSupply);
     }
